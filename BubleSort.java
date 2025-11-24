c40f1b6727c47e27c01ae775c97727f23b6121ff
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 18:06:46 -0600

    02 Se agrega el método principal

--- BubleSort.java
+++ BubleSort.java
@@ -1,3 +1,26 @@
+import java.io.*;
+
 public class BubleSort {
+
+    // Método principal de Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean intercambio;
+
+        for (int i = 0; i < n - 1; i++) {
+            intercambio = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    intercambio = true;
+                }
+            }
+            // Si no hubo intercambios, el arreglo ya está ordenado
+            if (!intercambio) break;
+        }
+    }
+
 
 }
